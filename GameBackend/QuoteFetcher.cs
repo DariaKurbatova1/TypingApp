cc74b5f7610f453f26417fe74a597c218faccd52
Daria Kurbatova <daria.kurbatova@dawsoncollege.qc.ca>
2024-11-11 15:15:43 -0500

    fix fetchQuotes method

--- GameBackend/QuoteFetcher.cs
+++ GameBackend/QuoteFetcher.cs
@@ -2,18 +2,19 @@ namespace GameBackend;
 using System.IO;
 using System.Text.Json.Nodes;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 public class QuoteFetcher
 {
     //add singleton pattern implementation to quote fetcher
-    public JsonObject fetchQuotes()
+    public JObject fetchQuotes()
     {
         string filePath = "H:\\projects\\TypingApp\\quotes.json";
         string jsonContent = File.ReadAllText(filePath);
         JsonQuote[] quotes = JsonConvert.DeserializeObject<JsonQuote[]>(jsonContent);
-        string json_data = JsonConvert.SerializeObject(quotes);
-        Console.WriteLine(json_data);
-        return null;
+        JObject jsonData = JObject.FromObject(new { quotes });
+        Console.WriteLine(jsonData);
+        return jsonData;
     }
 }
