af5f7161e18a308394202ad228b6d3041852a85f
Daria Kurbatova <daria.kurbatova@dawsoncollege.qc.ca>
2024-11-11 15:11:20 -0500

    add conversion from string to json data

--- GameBackend/QuoteFetcher.cs
+++ GameBackend/QuoteFetcher.cs
@@ -1,13 +1,19 @@
 namespace GameBackend;
 using System.IO;
 using System.Text.Json.Nodes;
+using Newtonsoft.Json;
 
-public class QuoteFetcher{
+
+public class QuoteFetcher
+{
     //add singleton pattern implementation to quote fetcher
-    public JsonObject fetchQuotes(){
-        StreamReader reader = new StreamReader("H:\\projects\\TypingApp\\quotes.json");
-        string line = reader.ReadToEnd();
-        Console.WriteLine(line);
+    public JsonObject fetchQuotes()
+    {
+        string filePath = "H:\\projects\\TypingApp\\quotes.json";
+        string jsonContent = File.ReadAllText(filePath);
+        JsonQuote[] quotes = JsonConvert.DeserializeObject<JsonQuote[]>(jsonContent);
+        string json_data = JsonConvert.SerializeObject(quotes);
+        Console.WriteLine(json_data);
         return null;
     }
 }
